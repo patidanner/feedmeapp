3568db76cc66317ef13178d135ef1a04068cf0b6
pdanner <patricia.danner@gmail.com>
2021-06-27 22:15:35 -0300

    Upload service rename and refactoring

--- src/main/java/com/inbound/text/feedMe/service/UploadService.java
+++ src/main/java/com/inbound/text/feedMe/service/UploadService.java
@@ -1,27 +1,26 @@
 package com.inbound.text.feedMe.service;
 
-import com.inbound.text.feedMe.model.Consumidor;
-import com.inbound.text.feedMe.model.Loja;
-import com.inbound.text.feedMe.model.Perfil;
 import com.inbound.text.feedMe.model.Staging;
-import com.inbound.text.feedMe.repository.ConsumidorRepository;
-import com.inbound.text.feedMe.repository.LojaRepository;
-import com.inbound.text.feedMe.repository.PerfilRespository;
 import com.inbound.text.feedMe.repository.StagingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.transaction.Transactional;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.*;
 
 @Service
-public class StagingService {
+public class UploadService {
 
+    private static String UPLOADED_DIR = "temp/";
     public static final String CPF = "cpf";
     public static final String PRIVATE = "private";
     public static final String INCOMPLETO = "incompleto";
@@ -30,50 +29,65 @@ public class StagingService {
     public static final String TICKET_ULTIMA_COMPRA = "ticketUltimaCompra";
     public static final String LOJA_MAIS_FREQUENTE = "lojaMaisFrequente";
     public static final String LOJA_ULTIMA_COMPRA = "lojaUltimaCompra";
-    @Autowired
-    private PerfilRespository perfilRespository;
-
-    @Autowired
-    private StagingServiceUtils serviceUtils;
 
     @Autowired
     private StagingRepository stagingRepository;
-
     @Autowired
-    private ConsumidorRepository consumidorRepository;
-
-    @Autowired
-    private LojaRepository lojaRepository;
+    private StagingServiceUtils serviceUtils;
 
     @Transactional
-    public Iterable<Staging> readAll() throws IOException, ParseException {
-        String file = "base_teste.txt";
+    public Boolean processFile(MultipartFile file) throws IOException, ParseException{
+        byte[] bytes = file.getBytes();
+        Path path = Paths.get(UPLOADED_DIR + file.getOriginalFilename());
+        Files.write(path, bytes);
+        this.processEntriesFromFile(file.getOriginalFilename());
+        return Boolean.TRUE;
+    }
 
+    @Transactional
+    public Iterable<Staging> processEntriesFromFile(String fileName) throws IOException, ParseException {
+        String fileDir;
+        fileDir = getFileDirectory(fileName);
         long t1 = System.currentTimeMillis();
-        Scanner scanner = new Scanner(new File(file), StandardCharsets.UTF_8);
+        Scanner scanner = new Scanner(new File(fileDir), StandardCharsets.UTF_8);
         Map<String,String> row = new HashMap<>();
         List<Map<String,String>> rows = new ArrayList<>();
-        scanner.nextLine();
-        while(scanner.hasNext()){
-            row = new HashMap<>();
-            row.put(CPF,scanner.next());
-            row.put(PRIVATE, scanner.next());
-            row.put(INCOMPLETO,scanner.next());
-            row.put(DATA_ULTIMA_COMPRA,scanner.next());
-            row.put(TICKET_MEDIO,scanner.next());
-            row.put(TICKET_ULTIMA_COMPRA,scanner.next());
-            row.put(LOJA_MAIS_FREQUENTE, scanner.next());
-            row.put(LOJA_ULTIMA_COMPRA, scanner.next());
-            rows.add(row);
-        }
+        this.readFileContent(scanner, rows);
         scanner.close();
         List<Staging> stagingList = new ArrayList<>();
         this.stagingPopulator(rows, stagingList);
         long t2 = System.currentTimeMillis();
-        Iterable<Staging> stagingSaveAll = this.saveAllStagingToDatabase(stagingList);
+        Iterable<Staging> databaseResult = this.saveAll(stagingList);
         long delta = t2-t1;
         System.out.println("Demorei "+ delta + " ms para processar o arquivo");
-        return stagingSaveAll;
+        return databaseResult;
+    }
+
+    private void readFileContent(Scanner scanner, List<Map<String, String>> rows) {
+        Map<String, String> row;
+        scanner.nextLine();
+        while(scanner.hasNext()){
+            row = new HashMap<>();
+            row.put(CPF, scanner.next());
+            row.put(PRIVATE, scanner.next());
+            row.put(INCOMPLETO, scanner.next());
+            row.put(DATA_ULTIMA_COMPRA, scanner.next());
+            row.put(TICKET_MEDIO, scanner.next());
+            row.put(TICKET_ULTIMA_COMPRA, scanner.next());
+            row.put(LOJA_MAIS_FREQUENTE, scanner.next());
+            row.put(LOJA_ULTIMA_COMPRA, scanner.next());
+            rows.add(row);
+        }
+    }
+
+    private String getFileDirectory(String fileName) {
+        String fileDir;
+        if(fileName == null){
+            fileDir = "base_teste.txt";
+        }else{
+            fileDir = UPLOADED_DIR + fileName;
+        }
+        return fileDir;
     }
 
     public void stagingPopulator(List<Map<String,String>> rows, List<Staging> stagingList) throws ParseException {
@@ -83,8 +97,8 @@ public class StagingService {
              rows) {
             staging = new Staging();
             staging.setCpf(row.get(CPF));
-            staging.setEstaIncompleto(row.get(INCOMPLETO).equalsIgnoreCase("1") ? Boolean.TRUE : Boolean.FALSE);
-            staging.setEstaPrivate(row.get(PRIVATE).equalsIgnoreCase("1") ? Boolean.TRUE : Boolean.FALSE);
+            staging.setIsIncompleto(row.get(INCOMPLETO).equalsIgnoreCase("1") ? Boolean.TRUE : Boolean.FALSE);
+            staging.setIsPrivate(row.get(PRIVATE).equalsIgnoreCase("1") ? Boolean.TRUE : Boolean.FALSE);
             staging.setLojaMaisFrequente(row.get(LOJA_MAIS_FREQUENTE));
             staging.setTicketUltimaCompra(!row.get(TICKET_ULTIMA_COMPRA).equalsIgnoreCase("null") ? format.parse(row.get(TICKET_ULTIMA_COMPRA)).doubleValue() : null);
             staging.setTicketMedio(!row.get(TICKET_MEDIO).equalsIgnoreCase("null") ? format.parse(row.get(TICKET_MEDIO)).doubleValue() : null);
@@ -97,25 +111,7 @@ public class StagingService {
     }
 
     @Transactional
-    public Staging saveToDatabase(Staging stagingData){
-        return stagingRepository.save(stagingData);
-    }
-
-    @Transactional
-    public Iterable<Perfil> saveAllPerfil(List<Perfil> perfilList){
-        return perfilRespository.saveAll(perfilList);
-    }
-    @Transactional
-    public Iterable<Consumidor> saveAllConsumidor(List<Consumidor> consumidorList){
-        return consumidorRepository.saveAll(consumidorList);
-    }
-    @Transactional
-    public Iterable<Loja> saveAllLoja(List<Loja> lojaList){
-        return lojaRepository.saveAll(lojaList);
-    }
-
-    @Transactional
-    public Iterable<Staging> saveAllStagingToDatabase(List<Staging> stagingList){
+    public Iterable<Staging> saveAll(List<Staging> stagingList){
         return stagingRepository.saveAll(stagingList);
     }
 
