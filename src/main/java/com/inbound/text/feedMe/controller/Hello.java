5c4f08e127608a57b931337328a542c1ce5cffef
pdanner <patricia.danner@gmail.com>
2021-06-27 22:17:22 -0300

    Changes controller from Rest to Controller; creates some upload web interface.

--- src/main/java/com/inbound/text/feedMe/controller/Hello.java
+++ src/main/java/com/inbound/text/feedMe/controller/Hello.java
@@ -1,23 +1,75 @@
 package com.inbound.text.feedMe.controller;
 
-import com.inbound.text.feedMe.model.Staging;
-import com.inbound.text.feedMe.service.StagingService;
+import com.inbound.text.feedMe.service.UploadService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.thymeleaf.util.ContentTypeUtils;
 
 import java.io.IOException;
 import java.text.ParseException;
 
-@RestController
+@Controller
 public class Hello {
 
+    public static final String BASE_TEST_TXT = "base_test.txt";
     @Autowired
-    private StagingService readFromFile;
+    private UploadService uploadService;
 
-    @GetMapping("/feedme")
-    public Iterable<Staging> readAll(@RequestParam(value = "name", defaultValue = "World") String name) throws IOException, ParseException {
-        return readFromFile.readAll();
+    @GetMapping("/")
+    public String index() {
+        return "upload";
+    }
+
+    @GetMapping("/feedmeexample")
+    public String  readAllFromDataExample(RedirectAttributes redirectAttributes) throws IOException, ParseException {
+        redirectAttributes.addFlashAttribute("message",
+                "Arquivo processado com sucesso!" +
+                        " Experimente ir até http://localhost:5050/ verificar o conteúdo da tabela staging.");
+
+        uploadService.processEntriesFromFile(null);
+        return "redirect:/uploadStatus";
+    }
+
+    @PostMapping("/feedme")
+    public String readAllFromUploadedFile(@RequestParam("file") MultipartFile file,
+                                                     RedirectAttributes redirectAttributes) throws IOException, ParseException {
+        String error = isValidFile(file, redirectAttributes);
+        if (error != null){
+            return error;
+        }
+        try{
+            uploadService.processFile(file);
+        }catch(Exception e){
+            redirectAttributes.addFlashAttribute("message", "Não consigo ler o conteúdo deste arquivo :( Por favor verifique o conteúdo dele.");
+            return "redirect:uploadStatus";
+        }
+        redirectAttributes.addFlashAttribute("message",
+                "Arquivo processado com sucesso!" +
+                        " Experimente ir até http://localhost:5050/ verificar o conteúdo da tabela staging.");
+
+        return "redirect:/uploadStatus";
+    }
+
+    @GetMapping("/uploadStatus")
+    public String uploadStatus() {
+        return "uploadStatus";
+
+    }
+
+    private String isValidFile(MultipartFile file, RedirectAttributes redirectAttributes) {
+        if (file.isEmpty()) {
+            redirectAttributes.addFlashAttribute("message", "Arquivo vazio!");
+            return "redirect:uploadStatus";
+        }
+        if (!ContentTypeUtils.isContentTypeText(file.getContentType())){
+            redirectAttributes.addFlashAttribute("message", "Por enquanto só posso processar aquivos .txt! Por favor, escolha outro um arquivo do tipo texto.");
+            return "redirect:uploadStatus";
+        }
+        return null;
     }
 }
