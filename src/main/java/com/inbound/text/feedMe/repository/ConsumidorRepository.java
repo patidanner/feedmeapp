ba2b5604bee2ef8f9c082bc709377b93c6390e09
pdanner <patricia.danner@gmail.com>
2021-06-27 22:16:08 -0300

    Removes unused entities

--- src/main/java/com/inbound/text/feedMe/repository/ConsumidorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.inbound.text.feedMe.repository;
-
-import com.inbound.text.feedMe.model.Consumidor;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConsumidorRepository extends CrudRepository<Consumidor,Long> {
-}
